681127fdc4b2203bcf5ec75b565219ab28dbbfd5
JD110225 <51008292+JD110225@users.noreply.github.com>
2020-06-10 20:20:07 -0600

    Decoder

--- tp2Prueba.cpp
+++ tp2Prueba.cpp
@@ -1,64 +1,80 @@
 #include <iostream>
+#include "List.h"
 using namespace std;
-class Lista
+class Decoder
 {
-private:
-    string cabeza;
-    string cola;
-
 public:
-    string getCabeza(){
-        return cabeza;
-    }
-    string getCola(){
-        return cola;
-    }
-    void removerInicioYFin(string &s)  //quitar el primero y el ultimo parentesis.
+    void removerInicioYFin(string& s)  //quitar el primero y el ultimo parentesis.
     {
         s.erase(0, 1);
         s.erase(s.length() - 1, 1);
     }
-    void setComponentes(string s) //hace el set de la lista y el de la cola, en un solo metodo para no copy pastear codigo.
+    bool esListaBasica(string s) {
+        return s.length() == 5 && s[0] == '(' && isdigit(s[1]) && s[2] == ' ' && isdigit(s[3]) && s[4] == ')';
+    }
+    bool esSoloCabeza(string s) {
+        return s.length() == 3 && s[0] == '(' && isdigit(s[1]) && s[2] == ')';
+    }
+    List<int>* stringToList(string s) //hace el set de la lista y el de la cola, en un solo metodo para no copy pastear codigo.
     {
-        removerInicioYFin(s);
-        int indiceFinal = 0; //indice donde termina la cabeza
-        string cabeza;
-        int counter = 0;
-        bool cabezaEncontrada = false;
-        for (int i = 0; i < s.length() && !cabezaEncontrada; ++i)
-        {
-            char c = s[i];
-            cabeza += c;
-            if (c == '(')
-            {
-                ++counter;
+        List<int>* l= nullptr;
+        List<int>* head = nullptr;
+        List<int>* tail = nullptr;
+        int valorAtomico = -1;
+        if (esSoloCabeza(s)) {
+            valorAtomico = (int)(s[1]-48);//Well, in ASCII code, the numbers(digits) start from 48. Stack Overflow es la biblia
+        }
+        else {
+            if (esListaBasica(s)) {
+                head = new List<int>((int)(s[1] - 48));  //deben ser pointers al parecer.
+                tail = new List<int>((int)(s[3] - 48));
             }
-            else
-            {
-                if (c == ')')
+            else {
+                removerInicioYFin(s);
+                string cabeza;
+                int indiceFinal = 0; //indice donde termina la cabeza
+                int counter = 0;
+                bool cabezaEncontrada = false;
+                for (int i = 0; i < s.length() && !cabezaEncontrada; ++i)
                 {
-                    if (counter == 1)
+                    char c = s[i];
+                    cabeza += c;
+                    if (c == '(')
                     {
-                        this->cabeza=cabeza;
-                        cabezaEncontrada = true;
-                        indiceFinal = i;
+                        ++counter;
                     }
                     else
                     {
-                        --counter;
+                        if (c == ')')
+                        {
+                            if (counter == 1)
+                            {
+                                head=stringToList(cabeza);
+                                cabezaEncontrada = true;
+                                indiceFinal = i;
+                            }
+                            else
+                            {
+                                --counter;
+                            }
+                        }
+
                     }
                 }
+                string cola = s.substr(indiceFinal + 1); //La cola va desde donde termina la cabeza, hasta el final de la lista
+                tail=stringToList(cola);
             }
         }
-        cola = s.substr(indiceFinal+1); //La cola va desde donde termina la cabeza, hasta el final de la lista
-    }
-};
+        l = new List<int>(head, tail, valorAtomico);
+        return l;
+     }
+        
+    };
 int main()
 {
-    string s ="((4)((1 3)(2 1))(1))";
-    Lista l;
-    l.setComponentes(s);
-    cout<<"Cabeza: "<<l.getCabeza()<<endl;
-    cout <<"Cola: "<<l.getCola()<<endl;
+    string s = "(((1 2)(3 4))(8 9))";
+    Decoder d;
+    List<int>* l=d.stringToList(s);
+    cout <<"La cola de la cabeza es: "<< l->getHead()->getTail()->toString();
     return 0;
 }
