4d87f1c5897b5a40ee84ef11a1ae8e9fe43cd162
KABU09 <55645024+KABU09@users.noreply.github.com>
2020-06-17 22:34:08 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,55 +1,31 @@
-/*****************************************************
-this main contains examples of how to use class List
-*****************************************************/
-#include <iostream>
-#include "List.h"
-int main(){
-    //atomic lists
-    //the name is optional
-    List<int> * list1 = new List<int>(3, "list1");
-    List<int> * list2 = new List<int>(5, "list2");
-    List<int> * list3 = new List<int>(6, "list3");
-    List<int> * list4 = new List<int>(7, "list4");
-    List<int> * list5 = new List<int>(10, "list5");
+#include "main.h"
+#include "Controlador.h"
 
-    //non atomic lists: lists that contains other lists
-    //the name is optional
-    List<int> *list6 = new List<int>(list1, list2, "list6", -1);
-    List<int> *list7 = new List<int>(list3, list4, "list7", -1);
-    List<int> *list8 = new List<int>(list6, list7, "list8", -1);
-    List<int> *list9 = new List<int>(list5, list8, "list9", -1);
+void run();
+int main() {
+	run();
+	return 0;
+}
 
-    //show lists
-    std::cout<<"Name: "<<list9->getName()<<std::endl;
-    std::cout<<list9->toString()<<std::endl;
-
-    //get head and tail of a list
-    std::cout<<"Head: "<<list9->getHead()->toString()<<std::endl;
-    std::cout<<"Tail: "<<list9->getTail()->toString()<<std::endl;
-
-    //search (atomic) values in a list
-    std::cout<<"Find -1: "<<list9->find(-1)<<std::endl;
-    std::cout<<"Find 10: "<<list9->find(10)<<std::endl;
-
-    //getting sublists by name
-    //Example 1
-    List<int> *aux = list9->getSubList("list0");
-    if(aux!=nullptr){
-        std::cout<<"List list0:  "<<aux->toString()<<std::endl;
-    }
-    else{
-        std::cout<<"List list0:  Doesn't exists"<<std::endl;
-    }
-    //Example 2
-    aux = list9->getSubList("list6");
-    if(aux!=nullptr){
-        std::cout<<"List list6:  "<<aux->toString()<<std::endl;
-    }
-    else{
-        std::cout<<"Doesn't exists"<<std::endl;
-    }
-
-    //free memory
-    delete list9;
-
+void run() {
+	Interfaz interfaz;
+	std::stringstream ss;
+	ss << " 1. Int \n 2. Char \n 3. Double \n 4. String \n";
+	interfaz.mostrarMensaje(ss.str());
+	int dataType = interfaz.pedirNumero("Digite la opcion correspondiente al dato: ");
+	std::cin.ignore();
+	std::fflush(stdin);
+	if (dataType == 1) {
+		Controlador <int> c(dataType);
+		c.run();
+	}else if (dataType == 2) {
+		Controlador <char> c(dataType);
+		c.run();
+	}else if (dataType == 3) {
+		Controlador <double> c(dataType);
+		c.run();
+	}else if (dataType == 4) {
+		Controlador <std::string> c(dataType);
+		c.run();
+	}
 }
