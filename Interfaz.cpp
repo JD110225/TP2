b25c0d2903f6f1497cce810509eda22ad2832d35
KABU09 <55645024+KABU09@users.noreply.github.com>
2020-06-17 22:31:57 -0600

    Update

--- Interfaz.cpp
+++ Interfaz.cpp
@@ -3,10 +3,21 @@
 std::string Interfaz::pedirComando(std::string mensaje){//check for empty string
     std::string comando = "";
     std::cout << mensaje << std::flush;
-    std::cin >> comando;
+    getline(std::cin, comando);
     return comando;
 }
 
 void Interfaz::mostrarError(std::string mensaje){
     std::cout << "ERROR, " << mensaje << std::endl;
+}
+
+void Interfaz::mostrarMensaje(std::string mensaje){
+    std::cout << mensaje << std::endl;
+}
+
+int Interfaz::pedirNumero(std::string mensaje){
+    int numero = 1;
+    std::cout << mensaje << std::flush;
+    std::cin >> numero;
+    return numero;
 }
