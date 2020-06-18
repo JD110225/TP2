492cfac1d39fe026745858d2789fe73bb0168479
KABU09 <55645024+KABU09@users.noreply.github.com>
2020-06-17 22:32:44 -0600

    Update

--- Terminal.cpp
+++ Terminal.cpp
@@ -1,12 +1,12 @@
 #include "Terminal.h"
 
-std::vector<std::string>& Terminal::separarInstruccion(std::string const &instruccion, char separador) {
+std::vector<std::string> Terminal::separarInstruccion(std::string const &instruccion, char separador) {	
 	std::vector<std::string> keyWords;	
 	std::stringstream ss(instruccion);
 	std::string keyWord = "";	
-	while (getline(ss,keyWord, ' ')) {
+	while (getline(ss,keyWord, separador)) {		
 		keyWords.push_back(keyWord);
-	}
+	}	
 	return keyWords;
 }
 
@@ -14,13 +14,15 @@ int Terminal::identificarComando(std::string const& comando){
 	return INSTRUCCIONES.find(comando);
 }
 
+
+//int main(){      //01234567891234567891234567
+//	Terminal t;
+//	std::string instrucciones = "lista1";
+//	std::vector<std::string> keyWords = t.separarInstruccion(instrucciones, '.');
+//	std::cout << keyWords.size() << std::endl;
+//	for (int i = 0; i < keyWords.size(); ++i) {
+//		std::cout << keyWords[i] << std::endl;
+//	}
 //
-//int main(){                    //01234567891234567891234567
-//	std::string instrucciones = "crear mostrar buscar";
-//	//std::string instrucciones = "cabeza cola";
-//	int x = instrucciones.find("buscar");
-//	//int x = instrucciones.find("cola");
-//	std::cout << x << std::endl;
-//	std::cout << instrucciones[x] << std::endl;
 //	return 0;
 //}
--- Terminal.h
+++ Terminal.h
@@ -6,7 +6,7 @@
 #include <vector>
 class Terminal{
 public:
-	std::vector<std::string>& separarInstruccion(std::string const &instruccion, char separador);
+	std::vector<std::string> separarInstruccion(std::string const &instruccion, char separador);
 	int identificarComando(std::string const& comando);
 private:
 	std::string const INSTRUCCIONES = "crear mostrar buscar";//0 = crear     6 = mostrar   14 = buscar
