919efa0a4961b8104c42aa5142ea67ba45c54d24
KABU09 <55645024+KABU09@users.noreply.github.com>
2020-06-19 15:56:52 -0600

    Update Terminal.h

--- Terminal.h
+++ Terminal.h
@@ -24,6 +24,6 @@ public:
 	std::vector<std::string> separarInstruccion(std::string const& instruccion, char separador);
 	int identificarComando(std::string const& comando);
 private:
-	std::string const INSTRUCCIONES = "crear mostrar buscar";//0 = crear     6 = mostrar   14 = buscar
+	std::string const INSTRUCCIONES = "crear mostrar buscar existe";//0 = crear     6 = mostrar   14 = buscar     21 = existe
 };
 
