ae1c3ed71e364e62040f6794f301d2e1b3556c40
KABU09 <55645024+KABU09@users.noreply.github.com>
2020-06-18 22:32:06 -0600

    Update Interfaz.cpp

--- Interfaz.cpp
+++ Interfaz.cpp
@@ -16,8 +16,24 @@ void Interfaz::mostrarMensaje(std::string mensaje){
 }
 
 int Interfaz::pedirNumero(std::string mensaje){
-    int numero = 1;
-    std::cout << mensaje << std::flush;
-    std::cin >> numero;
+    std::string stringNumero = "";
+    int numero = -1;
+    bool valido = true;
+    while (numero == -1) {
+        std::cout << mensaje << std::flush;
+        std::cin >> stringNumero;
+        for (char c : stringNumero) {
+            if (!isdigit(c)) {
+                valido = false;
+                numero = -1;
+            }
+            else {
+                numero = stoi(stringNumero);
+            }
+        }
+        if (numero == -1) {
+            mostrarError("Input Invalido");
+        }
+    }
     return numero;
 }
