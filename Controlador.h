9e8e2f08a9d194c0fbd37d83b51979d631b1aa3c
KABU09 <55645024+KABU09@users.noreply.github.com>
2020-06-18 22:30:18 -0600

    Update Controlador.h

--- Controlador.h
+++ Controlador.h
@@ -12,12 +12,14 @@ class Controlador {
 public:
 	~Controlador() {
 		delete decoder;
-		borrarListas();
+		//borrarListas();
 	}
 	Controlador(int tipoDato) {
 		decoder = new Decoder(tipoDato);//recordar hacer delete		
 	}
 	void run() {
+		std::cin.ignore();
+		std::fflush(stdin);
 		mostrarInstrucciones();
 		std::string comando = "";
 		while (comando != "salir") {
@@ -57,63 +59,149 @@ public:
 	void crearLista(std::vector<std::string> instrucciones) {
 
 		switch (instrucciones.size()) {//check size    crear,lista1,lista2.cabeza.cola,lista4.cola.cabeza
-		case 3:
-			//Jose Delgado
+		case 3:			
 			crearListaConParentesis(instrucciones);
 			break;
-		case 4:
-			//KABU09
+		case 4:			
 			crearListaConComandos(instrucciones);
 		default:
+			interfaz.mostrarError("Argumentos Invalido");
 			break;
 		}
 	}
 	void crearListaConParentesis(std::vector<std::string> instrucciones) {		
-		if (lista.find(instrucciones[1]) == lista.end()) {
-			if (decoder->esInputValido(instrucciones[2])) {
-				List<T>* nueva = decoder->stringToList<T>(instrucciones[2], instrucciones[1]);
-				lista.insert(std::pair<std::string, List<T>*>(instrucciones[1], nueva));
+		if (decoder->getDataTypeID() == 4) {
+			if (listaString.find(instrucciones[1]) == listaString.end()) {
+				if (decoder->esInputValido(instrucciones[2])) {
+					List<std::string>* nueva = decoder->stringToList2(instrucciones[2], instrucciones[1]);
+					listaString.insert(std::pair<std::string, List<std::string>*>(instrucciones[1], nueva));
+				}
+				else {
+					interfaz.mostrarError("Estructura Invalida de la Lista");
+				}
 			}
 			else {
-				interfaz.mostrarError("Estructura Invalida de la Lista");
+				interfaz.mostrarError("Ya existe una lista con ese nombre");
 			}
 		}
 		else {
-			interfaz.mostrarError("Ya existe una lista con ese nombre");
-		}
+			if (lista.find(instrucciones[1]) == lista.end()) {
+				if (decoder->esInputValido(instrucciones[2]) && matchInput(instrucciones[2],decoder->getDataTypeID())) {
+					List<T>* nueva = decoder->stringToList<T>(instrucciones[2], instrucciones[1]);
+					lista.insert(std::pair<std::string, List<T>*>(instrucciones[1], nueva));
+				}
+				else {
+					interfaz.mostrarError("Estructura Invalida de la Lista");
+				}
+			}
+			else {
+				interfaz.mostrarError("Ya existe una lista con ese nombre");
+			}
+		}		
 	}
 	void crearListaConComandos(std::vector<std::string> instrucciones) {
-		if (lista.find(instrucciones[1]) == lista.end()) {
-			std::vector<std::string> cabeza = terminal.separarInstruccion(instrucciones[2], '.');//check if real
-			std::vector<std::string> cola = terminal.separarInstruccion(instrucciones[3], '.');//check if real
-			List<T>* listaCabeza = getList(cabeza);//check data type
-			List<T>* listaCola = getList(cola);//check data type
-			if (listaCabeza && listaCola) {
-				List<T>* nueva = new List<T>(listaCabeza, listaCola, instrucciones[1], listaCabeza->getValue());//check T valu
-				lista.insert({ instrucciones[1], nueva });
-			}
+		if (decoder->getDataTypeID() == 4) {
+			if (listaString.find(instrucciones[1]) == listaString.end()) {
+				std::vector<std::string> cabeza = terminal.separarInstruccion(instrucciones[2], '.');//check if real
+				std::vector<std::string> cola = terminal.separarInstruccion(instrucciones[3], '.');//check if real
+				List<std::string>* listaCabeza = getList2(cabeza);//check data type
+				List<std::string>* listaCola = getList2(cola);//check data type
+				if (listaCabeza && listaCola) {
+					List<std::string>* nueva = new List<std::string>(listaCabeza, listaCola, instrucciones[1], listaCabeza->getValue());//check T valu
+					listaString.insert({ instrucciones[1], nueva });
+				}
 
+			}
+			else {
+				interfaz.mostrarError("Ya existe una lista con ese nombre");
+			}
 		}
 		else {
-			interfaz.mostrarError("Ya existe una lista con ese nombre");
+			if (lista.find(instrucciones[1]) == lista.end()) {
+				std::vector<std::string> cabeza = terminal.separarInstruccion(instrucciones[2], '.');//check if real
+				std::vector<std::string> cola = terminal.separarInstruccion(instrucciones[3], '.');//check if real
+				List<T>* listaCabeza = getList(cabeza);//check data type
+				List<T>* listaCola = getList(cola);//check data type
+				if (listaCabeza && listaCola) {
+					List<T>* nueva = new List<T>(listaCabeza, listaCola, instrucciones[1], listaCabeza->getValue());//check T valu
+					lista.insert({ instrucciones[1], nueva });
+				}
+
+			}
+			else {
+				interfaz.mostrarError("Ya existe una lista con ese nombre");
+			}
 		}
+		
 	}
 
+	bool matchInput(std::string const& hilera, int tipoDato) {
+		bool valido = true;
+		switch (tipoDato){
+		case 1:
+			valido = esNumero(hilera);
+			break;
+		case 3:
+			valido = esDouble(hilera);
+		default:
+			break;
+		}
+		return valido;
+	}
+
+	bool esNumero(std::string const& hilera) {
+		bool valido = true;
+		for (char c : hilera) {
+			if (!((isdigit(c)) || (c == '(') || (c == ')') || (c == ' '))) {
+				valido = false;
+			}
+		}
+		return valido;
+	}
+
+	bool esDouble(std::string const& hilera) {
+		bool valido = true;
+		for (char c : hilera) {
+			if (!((isdigit(c)) || (c == '(') || (c == ')') || (c == ' ') || (c == '.'))) {
+				valido = false;
+			}
+		}
+		return valido;
+	}
 	void mostrarListas() {
 		stringstream ss;
-		typename std::map<std::string, List<T>*>::iterator it;
-		for (it = lista.begin(); it != lista.end(); ++it) {
-			ss << it->first << "		" << it->second->toString() << "\n";
+		if (decoder->getDataTypeID() == 4) {
+			typename std::map<std::string, List<std::string>*>::iterator it;
+			for (it = listaString.begin(); it != listaString.end(); ++it) {
+				ss << it->first << "		" << it->second->toString() << "\n";
+			}
 		}
+		else {
+			typename std::map<std::string, List<T>*>::iterator it;
+			for (it = lista.begin(); it != lista.end(); ++it) {
+				ss << it->first << "		" << it->second->toString() << "\n";
+			}
+		}
+		
 		interfaz.mostrarMensaje(ss.str());
 	}
 
 	void buscar(std::vector<std::string> instrucciones) {
 		std::vector<std::string> buscar = terminal.separarInstruccion(instrucciones[1], '.');//check if real		
-		List<T>* aux = getList(buscar);
-		if (aux) {
-			interfaz.mostrarMensaje(aux->toString());
+		if (decoder->getDataTypeID() == 4) {
+			List<std::string>* aux1 = getList2(buscar);
+			if (aux1) {
+				interfaz.mostrarMensaje(aux1->toString());
+			}
 		}
+		else {
+			List<T>* aux2 = getList(buscar);
+			if (aux2) {
+				interfaz.mostrarMensaje(aux2->toString());
+			}
+		}
+		
+		
 
 	}
 
@@ -176,12 +264,67 @@ public:
 		return aux;
 	}
 
-	void borrarListas() {
-		typename std::map<std::string, List<T>*>::iterator it;
-		for (it = lista.begin(); it != lista.end(); ++it) {
-			delete it->second;
 
+	List<std::string>* getList2(std::vector<std::string> instrucciones) {
+		List <std::string>* aux = nullptr;
+		if (listaString.find(instrucciones[0]) != listaString.end()) {
+			int opcion = 0;
+			aux = listaString[instrucciones[0]];
+			for (int i = 1; i < instrucciones.size() && aux; ++i) {
+				opcion = PARTESLISTA.find(instrucciones[i]);//check for npos
+				switch (opcion) {
+				case 0:
+				{
+					if (aux->getHead()) {
+						aux = aux->getHead();
+					}
+					else {
+						aux = nullptr;
+						interfaz.mostrarError("No existe la lista elegida como argumento");
+					}
+					break;
+				}
+				case 7:
+				{
+					if (aux) {//optimizacion
+						if (aux->getTail()) {
+							aux = aux->getTail();
+						}
+						else {
+							aux = nullptr;
+							interfaz.mostrarError("No existe la lista elegida como argumento");
+						}
+					}
+
+					break;
+				}
+				default:
+					break;
+				}
+			}
 		}
+		else {
+			interfaz.mostrarError("No existe la lista elegida como argumento");
+		}
+		return aux;
+	}
+
+	void borrarListas() {
+		if (decoder->getDataTypeID() == 4) {
+			typename std::map<std::string, List<std::string>*>::iterator it;
+			for (it = listaString.begin(); it != listaString.end(); ++it) {
+				delete it->second;
+
+			}
+		}
+		else {
+			typename std::map<std::string, List<T>*>::iterator it;
+			for (it = lista.begin(); it != lista.end(); ++it) {
+				delete it->second;
+
+			}
+		}
+		
 	}
 
 
@@ -189,6 +332,7 @@ public:
 private:
 	//1 int  2 char   3 Double   4 String
 	std::map<std::string, List<T>*> lista;
+	std::map<std::string, List<std::string>*> listaString;
 	std::string const PARTESLISTA = "cabeza cola";
 	Interfaz interfaz;
 	Terminal terminal;
